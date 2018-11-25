94acd9049c7b42b40bde6369e735a1182dd3e061
Sébastien Saunier <seb@saunier.me>
2018-11-25 19:13:55 +0100

    Add Jenkinsfile

--- morse.py
+++ morse.py
@@ -1,42 +1,43 @@
 class Morse:
-    ALPHABET = {
-        '.-':   'A',
-        '-...': 'B',
-        '-.-.': 'C',
-        '-..':  'D',
-        '.':    'E',
-        '..-.': 'F',
-        '--.':  'G',
-        '....': 'H',
-        '..':   'I',
-        '.---': 'J',
-        '-.-':  'K',
-        '.-..': 'L',
-        '--':   'M',
-        '-.':   'N',
-        '---':  'O',
-        '.--.': 'P',
-        '--.-': 'Q',
-        '.-.':  'R',
-        '...':  'S',
-        '-':    'T',
-        '..-':  'U',
-        '...-': 'V',
-        '.--':  'W',
-        '-..-': 'X',
-        '-.--': 'Y',
-        '--..': 'Z'
-    }
+    # pass
+    # ALPHABET = {
+    #     '.-':   'A',
+    #     '-...': 'B',
+    #     '-.-.': 'C',
+    #     '-..':  'D',
+    #     '.':    'E',
+    #     '..-.': 'F',
+    #     '--.':  'G',
+    #     '....': 'H',
+    #     '..':   'I',
+    #     '.---': 'J',
+    #     '-.-':  'K',
+    #     '.-..': 'L',
+    #     '--':   'M',
+    #     '-.':   'N',
+    #     '---':  'O',
+    #     '.--.': 'P',
+    #     '--.-': 'Q',
+    #     '.-.':  'R',
+    #     '...':  'S',
+    #     '-':    'T',
+    #     '..-':  'U',
+    #     '...-': 'V',
+    #     '.--':  'W',
+    #     '-..-': 'X',
+    #     '-.--': 'Y',
+    #     '--..': 'Z'
+    # }
 
-    def decode(self, input):
-        if input == "":
-            return ""
+    # def decode(self, input):
+    #     if input == "":
+    #         return ""
 
-        words = input.split(" / ")
-        decoded_words = []
-        for word in words:
-            symbols = word.split(" ")
-            letters = [ self.ALPHABET[s] for s in symbols ]
-            decoded_words.append(''.join(letters))
+    #     words = input.split(" / ")
+    #     decoded_words = []
+    #     for word in words:
+    #         symbols = word.split(" ")
+    #         letters = [ self.ALPHABET[s] for s in symbols ]
+    #         decoded_words.append(''.join(letters))
 
-        return ' '.join(decoded_words)
+    #     return ' '.join(decoded_words)
