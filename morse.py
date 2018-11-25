f2b4735fa081ac0f8ca5a20bae046f54f58aee33
Sébastien Saunier <seb@saunier.me>
2018-11-25 19:47:24 +0100

    100% passing tests for one-word Morse decoder

--- morse.py
+++ morse.py
@@ -1,43 +1,46 @@
 class Morse:
-    pass
-    # ALPHABET = {
-    #     '.-':   'A',
-    #     '-...': 'B',
-    #     '-.-.': 'C',
-    #     '-..':  'D',
-    #     '.':    'E',
-    #     '..-.': 'F',
-    #     '--.':  'G',
-    #     '....': 'H',
-    #     '..':   'I',
-    #     '.---': 'J',
-    #     '-.-':  'K',
-    #     '.-..': 'L',
-    #     '--':   'M',
-    #     '-.':   'N',
-    #     '---':  'O',
-    #     '.--.': 'P',
-    #     '--.-': 'Q',
-    #     '.-.':  'R',
-    #     '...':  'S',
-    #     '-':    'T',
-    #     '..-':  'U',
-    #     '...-': 'V',
-    #     '.--':  'W',
-    #     '-..-': 'X',
-    #     '-.--': 'Y',
-    #     '--..': 'Z'
-    # }
+    ALPHABET = {
+        '.-':   'A',
+        '-...': 'B',
+        '-.-.': 'C',
+        '-..':  'D',
+        '.':    'E',
+        '..-.': 'F',
+        '--.':  'G',
+        '....': 'H',
+        '..':   'I',
+        '.---': 'J',
+        '-.-':  'K',
+        '.-..': 'L',
+        '--':   'M',
+        '-.':   'N',
+        '---':  'O',
+        '.--.': 'P',
+        '--.-': 'Q',
+        '.-.':  'R',
+        '...':  'S',
+        '-':    'T',
+        '..-':  'U',
+        '...-': 'V',
+        '.--':  'W',
+        '-..-': 'X',
+        '-.--': 'Y',
+        '--..': 'Z'
+    }
 
-    # def decode(self, input):
-    #     if input == "":
-    #         return ""
+    def decode(self, input):
+        if input == "":
+            return ""
 
-    #     words = input.split(" / ")
-    #     decoded_words = []
-    #     for word in words:
-    #         symbols = word.split(" ")
-    #         letters = [ self.ALPHABET[s] for s in symbols ]
-    #         decoded_words.append(''.join(letters))
+        symbols = input.split(" ")
+        letters = [ self.ALPHABET[s] for s in symbols ]
+        return ''.join(letters)
 
-    #     return ' '.join(decoded_words)
+        # words = input.split(" / ")
+        # decoded_words = []
+        # for word in words:
+        #     symbols = word.split(" ")
+        #     letters = [ self.ALPHABET[s] for s in symbols ]
+        #     decoded_words.append(''.join(letters))
+
+        # return ' '.join(decoded_words)
